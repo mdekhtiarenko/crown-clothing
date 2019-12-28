08ce045715a58c1c3376b0b314990678f23e71fc
Mykola Dekhtiarenko <Mykola_Dekhtiarenko@epam.com>
2019-12-28 23:05:02 +0200

    add heder component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import HomePage from "./pages/homepage/homepage.component";
 import {Route, Switch} from "react-router-dom";
 import HatsPage from "./pages/hatspage/hats-page.component";
 import ShopPage from "./pages/shoppage/shop-page.component";
+import Header from "./components/header/header.component";
 
 function App() {
     return (
         <div>
+            <Header/>
             <Switch>
                 <Route exact path='/' component={HomePage}/>
                 <Route path='/shop' component={ShopPage}/>
