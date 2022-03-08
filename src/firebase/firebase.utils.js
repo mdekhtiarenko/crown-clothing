80f89a5ac2122613f55c5cddbbcda49a2a0fcf78
Mykola Dekhtiarenko <m.dekhtiarenko@ukma.edu.ua>
2022-03-08 18:57:15 +0200

    update dependencies

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCgmQ-DWa5XuWAwy5ivzCIrE-Lja72rXpM",
